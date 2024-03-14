e801057d439dcccba68a4c3b96085056d423e8ae
Dipan46 <57986902+Dipan46@users.noreply.github.com>
2024-03-14 16:35:29 +0530

    Adding animation

--- src/Component/Heading.js
+++ src/Component/Heading.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Heading() {
     return (
         <>
-            <h1 className='title'>Enter your text here:</h1>
+            <h1 className='title animate__animated animate__bounceIn'>Enter your text here:</h1>
         </>
     )
 }
--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Navbar(props) {
     return (
-        <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
+        <nav className="navbar navbar-expand-lg bg-body-tertiary animate__animated animate__jackInTheBox" data-bs-theme="dark">
             <div className="container-fluid">
                 <a className="navbar-brand" href="#">TextED</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
