4d052aa8cba1da45f273c2b2e82588b7f9e6edfa
Dipan46 <57986902+Dipan46@users.noreply.github.com>
2024-07-05 14:09:53 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Navbar from './Component/Navbar';
+import Body from './Component/Body'
+import Heading from './Component/Heading';
+import Button from './Component/Button';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Heading />
+      <Body />
+    </>
+  );
+}
+
+export default App;
